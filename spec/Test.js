0ecf879aa2e89c59d1a736f69e20e07896d08b11
sa <satish860@gmail.com>
2014-10-25 22:45:55 +0100

    Updated the Package files

--- spec/Test.js
+++ spec/Test.js
@@ -1,19 +1,1 @@
-(function () {
-  'use strict';
 
-  angular
-    .module('todoModule')
-    .factory('someFactory', someFactory);
-
-  /* @ngInject */
-  function someFactory($http) {
-    var service = {
-      Completed: Completed
-    };
-    return service;
-
-    ////////////////
-
-    function Completed() {}
-  }
-})();
